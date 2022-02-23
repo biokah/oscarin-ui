871accd7e96d86d8327d090438b766da100d2b28
Ivan Mayoral <iimayoral@gmail.com>
2022-02-23 15:49:59 -0600

    linting on files, createad pages/_document.js with common header

--- pages/ranking.js
+++ pages/ranking.js
@@ -1,24 +1,22 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Layout } from '../components/layout/Layout';
+import styles from "../styles/Home.module.css";
+import { Layout } from "../components/layout/Layout";
 
 export default function Ranking() {
-    return (
-        <>
-          <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600&display=swap" rel="stylesheet" />
-          </Head>
-          <div className={styles.container}>
-            <Layout>
-                <p>La ceremonia de los Premios Óscar se llevará a cabo el domingo 27 de marzo de 2022. Durante el evento, actualizaremos los resultados tan pronto como se vayan anunciando, y este ranking se actualizará conforme a los resultados de los participantes.</p>
-                <table>
-
-                </table>
-            </Layout>
-          </div>
-        </>
-    )
-  }
+  return (
+    <>
+      <div className={styles.container}>
+        <Layout>
+          <p>
+            La ceremonia de los Premios Óscar se llevará a cabo el domingo 27 de
+            marzo de 2022. Durante el evento, actualizaremos los resultados tan
+            pronto como se vayan anunciando, y este ranking se actualizará
+            conforme a los resultados de los participantes.
+          </p>
+          <table></table>
+        </Layout>
+      </div>
+    </>
+  );
+}
