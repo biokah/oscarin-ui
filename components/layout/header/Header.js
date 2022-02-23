dcda8a3e47e06b87b5e456d85f332e4a74e8c4de
Maria Anais Reyes <maria.anaisr@gmail.com>
2022-02-23 11:23:16 -0600

    Adding route params to header component

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -2,13 +2,16 @@ import Image from 'next/image'
 import logo from './../../../assets/images/Oscarin_Logo.png'
 import send from './../../../assets/images/send.png'
 import Link from 'next/link'
+import {useRouter} from 'next/router';
+
 
 export const Header = () => {
+  const router = useRouter()
 
     return (
       <header className="c_header absolute bg-white top-0 left-0 right-0 flex items-center justify-between">
         <section className="flex">
-          <Link href="/" className="logo" >
+          <Link href="/">
             <Image
               src={logo}
               alt="logo"
@@ -17,16 +20,15 @@ export const Header = () => {
           </Link>
           <nav className="nav">
             <ul className="flex">
-              <li className="active rounded-lg" >
+              <li className={`rounded-lg ${router.pathname === "/" ? "active" : ""}`}>
                 <Link href="/">Mi quiniela</Link>
               </li>
-              <li>
+              <li className={`rounded-lg ${router.pathname === "/ranking" ? "active" : ""}`}>
                 <Link href="/ranking">Ranking</Link>
               </li>
-              <li>
+              <li className={`rounded-lg ${router.pathname === "/rules" ? "active" : ""}`}>
                 <Link href="/rules">Reglas</Link>
               </li>
-              
             </ul>
           </nav>
         </section>
--- pages/ranking.js
+++ pages/ranking.js
@@ -12,7 +12,12 @@ export default function Ranking() {
             <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600&display=swap" rel="stylesheet" />
           </Head>
           <div className={styles.container}>
-            <Layout />
+            <Layout>
+                <p>La ceremonia de los Premios Óscar se llevará a cabo el domingo 27 de marzo de 2022. Durante el evento, actualizaremos los resultados tan pronto como se vayan anunciando, y este ranking se actualizará conforme a los resultados de los participantes.</p>
+                <table>
+
+                </table>
+            </Layout>
           </div>
         </>
     )
